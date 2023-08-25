36525921813e8e6fdca81f5c98a9a6561e6104ac
GIOVANNA SILVA BELO <23111786@CFP127ALUNO.LOCAL>
2023-08-25 15:25:25 -0300

    não terminado

--- aula03.js
+++ aula03.js
@@ -1,9 +1,12 @@
 'use strict'
 
+
 const botaoSomar = document.getElementById('somar')
 const botaoIdentificar = document.getElementById('identificar')
 const botaoMostrar = document.getElementById('mostrar')
-const botaoSoma2 = document.getElementById('somaNumeros')
+const botaoSoma = document.getElementById('somaDeNumeros')
+
+
 
 
 function somar (){
@@ -11,12 +14,16 @@ function somar (){
     const numero2 = Number (document.getElementById('numero2').value)
     const resultado = document.getElementById('resultado')
 
+
     const total = numero1 + numero2
     resultado.textContent = total
 
+
 }
 
+
 ///////////////////////////////////////////////////////////////////////////
+
 
 function identificar () {
     const numero = Number(document.getElementById('numero-item2').value)
@@ -30,7 +37,9 @@ function identificar () {
     }
 }
 
+
 /////////////////////////////ATIVIDADE  D/////////////////////////////////////////
+
 
 function mostrar () {
     const nota1 = Number(document.getElementById('nota1').value)
@@ -40,18 +49,18 @@ function mostrar () {
     const nota5 = Number(document.getElementById('nota5').value)
     const nota5texto = document.getElementById('notaDiv')
 
+
     const resultado = document.getElementById('resultado-item3')
     const resultadoMedia = document.getElementById('resultado-media')
 
-    
-    
-    
-
     if (resultado.textContent == 'EM EXAME!'){
+
 
         const media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
 
+
         if (media >= 7){
+
 
             resultado.textContent = 'APROVADO EM EXAME!'
         } else{
@@ -59,14 +68,18 @@ function mostrar () {
             nota5texto.classList.remove ('nota5')
         }
 
+
         resultadoMedia.textContent = media
 
+
     } else {
+
 
         const media = (nota1 + nota2 + nota3 + nota4) / 4
         resultadoMedia.textContent = media
              
         if (media >= 7){
+
 
             resultado.textContent = 'APROVADO !'
         } else{
@@ -76,11 +89,34 @@ function mostrar () {
     }
 }
 
+
 /////////////////////////////ATIVIDADE  N/////////////////////////////////////////
 
-function somaDeNumeros () {
 
+function somaNumeros () {
+    const valorA = Number(document.getElementById('valorA').value)
+    const valorB = Number(document.getElementById('valorB').value)
+    const valorC = Number(document.getElementById('valorC').value)
+    const soma = document.getElementById('soma-item3')
+
+
+
+
+    const resultado = valorA + valorB + valorC
+
+
+    if (resultado >= 100){
+
+
+        soma.textContent = resultado
+       
+
+
+    } else{
+        soma.textContent = 'Sem resposta!'
+    }
 }
+
 
 
 
@@ -89,8 +125,4 @@ function somaDeNumeros () {
 botaoSomar.addEventListener('click', somar)
 botaoIdentificar.addEventListener('click', identificar)
 botaoMostrar.addEventListener('click', mostrar)
-botaoSoma2.addEventListener('click', somaDeNumeros)
-
-
-
-
+botaoSoma.addEventListener('click', somaNumeros)
